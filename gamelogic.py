c1c7327a7127fffdbf6b3efb4dad89244a430766
SHIMHUN <shimhun88@naver.com>
2024-07-03 15:14:24 +0900

    task2

--- gamelogic.py
+++ gamelogic.py
@@ -29,13 +29,16 @@ class GameLogic:
         self.paddle_right.reset()
 
     def ball_falls_left(self): # <-- TODO: complete this function. check if self.ball.position[0] goes below 0
-        return False
+        if self.ball.position[0] <= 0:
+            return False
 
     def ball_falls_right(self): # <-- TODO: complete this function. check if self.ball.position[0] exceeds WIDTH
-        return False
+        if self.ball.position[0] >= WIDTH:
+            return False
 
     def ball_hits_wall(self): # <-- TODO: complete this function. check if self.ball.position[1] goes beyond 0 or HEIGHT
-        return False
+        if (self.ball.position[1] <= 0 or self.ball.position[1] >= HEIGHT):
+            return True
 
     def ball_hits_paddle(self):
         return self.ball.is_collision(self.paddle_left) or self.ball.is_collision(self.paddle_right)
@@ -58,4 +61,8 @@ class GameLogic:
         IF ball hits paddle
             - X-axis velocity reverses
         """
-
+        
+        if self.ball_hits_wall():
+            self.ball.velocity[1] *= -1
+        if self.ball_hits_paddle():
+            self.ball.velocity[0] *= -1
